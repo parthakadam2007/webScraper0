5cc60c614ea0391a8b2c7eab9824806cd9016709
Partha Kadam <parthakadam2007@gmail.com>
2025-12-17 20:48:53 +0530

    inital commit

---
+++ src/main/kotlin/com/example/webScraper0/WebScraper0Application.kt
@@ -0,0 +1,11 @@
+package com.example.webScraper0
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebScraper0Application
+
+fun main(args: Array<String>) {
+	runApplication<WebScraper0Application>(*args)
+}
---
+++ src/test/kotlin/com/example/webScraper0/WebScraper0ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.webScraper0
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebScraper0ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
